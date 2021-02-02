6554a414954df22a176e73db2c741b444b7190a6
Joseph Itiose <rexgmann@gmail.com>
2021-02-02 04:03:53 +0100

    message

--- web/shoppingCart/viewCart.php
+++ web/shoppingCart/viewCart.php
@@ -29,3 +29,31 @@ echo "We are here"; ?>
 						<th style="text-align:right;"><strong>Price</strong></th>
 						<th style="text-align:center;"><strong>Action</strong></th>
 					</tr>	
+
+					<?php		
+						foreach ($_SESSION["cart_item"] as $item){
+							?>
+									<tr>
+										<td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><strong><?php echo $item["name"]; ?></strong></td>
+										<td style="text-align:left;border-bottom:#F0F0F0 1px solid;"><?php echo $item["code"]; ?></td>
+										<td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo $item["quantity"]; ?></td>
+										<td style="text-align:right;border-bottom:#F0F0F0 1px solid;"><?php echo "$".$item["price"]; ?></td>
+										<td style="text-align:center;border-bottom:#F0F0F0 1px solid;"><a href="index.php?action=remove&code=<?php echo $item["code"]; ?>" class="btnRemoveAction">Remove Item</a></td>
+									</tr>
+									<?php
+							$item_total += ($item["price"]*$item["quantity"]);
+							}
+							?>
+
+					<tr>
+						<td colspan="5"><strong>Total:</strong> <?php echo "$" . $item_total; ?></td>
+					</tr>
+				</tbody>
+			</table>		
+
+			</div>
+		</div>
+			<div class="txt-headingout"><a id="btnCheckout" href="index.php?action=checkout">Check out</a></div>
+		</main>
+</body>
+</html>
