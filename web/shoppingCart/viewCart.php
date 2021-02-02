6840b8a7dc1689c4a45cd3de69920905927c337d
Joseph Itiose <rexgmann@gmail.com>
2021-02-02 03:57:03 +0100

    message

--- web/shoppingCart/viewCart.php
+++ web/shoppingCart/viewCart.php
@@ -14,3 +14,18 @@ echo "We are here"; ?>
 	<main>
 		<div id="shopping-cart">
 			<div class="txt-heading">Shopping Cart <a id="btnView" href="index.php?action=browse">Browse</a><a id="btnEmpty" href="index.php?action=empty">Empty Cart </a></div>
+
+			<?php
+			echo "we are here again";
+				if(isset($_SESSION["cart_item"])){
+					$item_total = 0;
+			?>	
+			<table cellpadding="10" cellspacing="1">
+				<tbody>
+					<tr>
+						<th style="text-align:left;"><strong>Name</strong></th>
+						<th style="text-align:left;"><strong>Code</strong></th>
+						<th style="text-align:right;"><strong>Quantity</strong></th>
+						<th style="text-align:right;"><strong>Price</strong></th>
+						<th style="text-align:center;"><strong>Action</strong></th>
+					</tr>	
