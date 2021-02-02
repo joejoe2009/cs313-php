6fb665f3f41954672c158357bf2b30252b7f7bd3
Joseph Itiose <rexgmann@gmail.com>
2021-02-02 03:54:11 +0100

    message

--- web/shoppingCart/viewCart.php
+++ web/shoppingCart/viewCart.php
@@ -1,3 +1,16 @@
 <?php
 session_start();
 echo "We are here"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cart</title>
+    <link href="css/shcar.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<main>
+		<div id="shopping-cart">
+			<div class="txt-heading">Shopping Cart <a id="btnView" href="index.php?action=browse">Browse</a><a id="btnEmpty" href="index.php?action=empty">Empty Cart </a></div>
