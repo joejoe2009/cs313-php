4128214f8fd7afa90b631c4a42b9d038b7ba6bcd
Joseph Itiose <rexgmann@gmail.com>
2021-02-13 02:38:02 +0100

    new message

--- web/week5/students.php
+++ web/week5/students.php
@@ -1,8 +1,8 @@
 <?php
-//require 'connection.php';
-//get_db() function created in connection.php
-//$db = get_db();
-//session_start();
+require 'connection.php';
+get_db() function created in connection.php
+$db = get_db();
+session_start();
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,7 +12,7 @@
 
 <body>
 <?php
-/*$statement = $db->prepare("SELECT name, username, password FROM students");
+$statement = $db->prepare("SELECT name, username, password FROM students");
 $statement->execute();
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             {
@@ -23,9 +23,8 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
             $username = $row['username'];
             $password = $row['password'];
             echo "<p><strong>$name $username $password</strong><p>";
-            } */
+            } 
 
 ?>
-testing this page
 </body>
 </html>
