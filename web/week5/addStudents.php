093837e260a80652e0d81cf7897589579ab5b817
Joseph Itiose <rexgmann@gmail.com>
2021-02-21 05:11:03 +0100

    new message

--- web/week5/addStudents.php
+++ web/week5/addStudents.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+var_dump($_POST);
+exit;
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
