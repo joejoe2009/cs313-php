4da3eccf2991415729ac6944ec72134fb44194d8
Joseph Itiose <rexgmann@gmail.com>
2021-02-21 05:06:23 +0100

    new message

--- web/week5/addStudents.php
+++ web/week5/addStudents.php
@@ -1,7 +1,9 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
 
--- web/week5/students.php
+++ web/week5/students.php
@@ -1,8 +1,6 @@
 <?php
 require_once '/app/web/model/main-model.php';
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 //get_db() function created in connection.php
 session_start();
