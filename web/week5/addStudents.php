2ed27c60089b078c3302a8bb650205c84636f736
Joseph Itiose <rexgmann@gmail.com>
2021-02-23 01:44:17 +0100

    new message

--- web/week5/addStudents.php
+++ web/week5/addStudents.php
@@ -1,5 +1,5 @@
 <?php
-
+include ("./main-model.php");
 $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
 $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_STRING);
