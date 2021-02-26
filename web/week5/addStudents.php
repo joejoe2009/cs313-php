9541499bc477a7fdcea578608b50d7b9c343d916
Joseph Itiose <rexgmann@gmail.com>
2021-02-26 04:14:03 +0100

    new message

--- web/week5/addStudents.php
+++ web/week5/addStudents.php
@@ -1,24 +1,11 @@
 <?php
-//include ('/app/web/model/main-model.php');
+include ('/app/web/model/main-model.php');
 require_once '/app/web/week5/connection.php';
 $name = $_POST['name'];
 $username = $_POST['username'];
 $password =  $_POST['password'];
 
-echo $name;
-function insertstudents($name, $username, $password){
-    $db = get_db();
-    $sql = 'INSERT INTO students (name, username, password) VALUES (:name, :username, :password )';
-    $stmt = $db->prepare($sql);
-    $stmt->bindValue(':name', $name, PDO::PARAM_STR);
-    $stmt->bindValue(':username', $username, PDO::PARAM_STR);
-    $stmt->bindValue(':password', $password, PDO::PARAM_STR);
-    $stmt->execute();
-     $rowsChanged = $stmt->rowCount();
-     $stmt->closeCursor();
-     echo "insertStudents";
-    return $rowsChanged;
-}
+
 
 
 $success = insertstudents($name, $username, $password);
